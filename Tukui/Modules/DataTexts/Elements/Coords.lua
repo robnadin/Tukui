4e0ecf1530029b806f7a93ece84f920119ac2f25
Tukz <tukz@tukui.org>
2019-08-10 16:54:18 -0400

    Update Coords.lua

--- Tukui/Modules/DataTexts/Elements/Coords.lua
+++ Tukui/Modules/DataTexts/Elements/Coords.lua
@@ -25,7 +25,7 @@ local Update = function(self, Elapsed)
 		X = 100 * math.floor(X * 10000) / 10000
 		Y = 100 * math.floor(Y * 10000) / 10000
 		
-		self.Text:SetText(X..", "..Y)
+		self.Text:SetText(DataText.ValueColor..X.."|r, "..DataText.ValueColor..Y.."|r")
 		
 		Timer = Interval
 	end
