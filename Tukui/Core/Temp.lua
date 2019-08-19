868e8f86584e9d3b846fcedfbc1f9231ff7852d9
Tukz <tukz@tukui.org>
2019-08-19 19:00:23 -0400

    Update Temp.lua

--- Tukui/Core/Temp.lua
+++ Tukui/Core/Temp.lua
@@ -3,3 +3,14 @@
 ----------------------------------
 
 local T, C, L = select(2, ...):unpack()
+
+--[[
+
+This is a TODO list that will be implemented after WoW Classic launch.
+
+1- Add Enemy Cast Bar to nameplates and unit frames via LibClassicCasterino.
+2- Add Keyring bag to micromenu.
+3- Display real mobs health value instead of pourcent via LibClassicMobHealth-1.0.
+4- Add Bags sorting button.
+
+--]]
