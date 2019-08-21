eb0d6b94458e5ee058adf47e2351b8843f716676
Tukz <tukz@tukui.org>
2019-08-20 21:47:47 -0400

    This is moved into changelog.

--- Tukui/Core/Temp.lua
+++ Tukui/Core/Temp.lua
@@ -3,15 +3,3 @@
 ----------------------------------
 
 local T, C, L = select(2, ...):unpack()
-
---[[
-
-This is a TODO list that will be implemented after WoW Classic launch.
-
-1- Add Enemy Cast Bar to nameplates and unit frames via LibClassicCasterino.
-2- Add Keyring bag to micromenu.
-3- Rewrite UnitFrames PostUpdatePower and PostUpdateHealth. Use LibClassicMobHealth-1.0.
-4- Add Bags sorting button.
-5- Add buttons to land at the next available flight master
-
---]]
