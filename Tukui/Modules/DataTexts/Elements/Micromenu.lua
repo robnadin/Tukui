90d739f6974152fe70b9030edea71418ec78e640
Tukz <tukz@tukui.org>
2019-09-01 09:37:27 -0400

    Add a micro menu datatext.

---
+++ Tukui/Modules/DataTexts/Elements/Micromenu.lua
@@ -0,0 +1,20 @@
+local T, C, L = select(2, ...):unpack()
+
+local DataText = T["DataTexts"]
+local Popups = T["Popups"]
+local Miscellaneous = T.Miscellaneous
+
+local OnMouseDown = function()
+	Miscellaneous.DropDown.Open(Miscellaneous.MicroMenu.Buttons, Miscellaneous.MicroMenu, "cursor", 0, 0, "MENU", 2)
+end
+
+local Enable = function(self)
+	self:SetScript("OnMouseDown", OnMouseDown)
+	self.Text:SetText("Micro Menu")
+end
+
+local Disable = function(self)
+	self:SetScript("OnMouseDown", nil)
+end
+
+DataText:Register("Micro Menu", Enable, Disable, Update)
