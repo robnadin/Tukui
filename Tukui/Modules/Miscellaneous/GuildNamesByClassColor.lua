d87a6ae218b560fd9a8c61825e07e84f61ec9677
Tukz <tukz@tukui.org>
2019-10-16 12:04:40 -0400

    No need to localize Class arg, GetGuildRosterInfo already have an arg for that.

--- Tukui/Modules/Miscellaneous/GuildNamesByClassColor.lua
+++ Tukui/Modules/Miscellaneous/GuildNamesByClassColor.lua
@@ -9,14 +9,8 @@ function GuildNamesByClassColor:Update()
 		
 		for i=1, GUILDMEMBERS_TO_DISPLAY, 1 do
 			local Index = Offset + i
-			local FullName, _, _, _, Class, _, _, _, Online = GetGuildRosterInfo(Index)
+			local FullName, _, _, _, _, _, _, _, Online, _, Class = GetGuildRosterInfo(Index)
 			local Button = _G["GuildFrameButton"..i.."Name"]
-			
-			for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
-				if Class == v then
-					Class = k
-				end
-			end
 
 			local R, G, B = unpack(T.Colors.class[Class])
 			local Hex = T.RGBToHex(R, G, B)
