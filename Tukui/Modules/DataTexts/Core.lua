0968a4b7499502d10101c89cf1985bec5f6e5c71
Tukz <tukz@tukui.org>
2020-04-05 16:42:09 -0400

    Update Core.lua

--- Tukui/Modules/DataTexts/Core.lua
+++ Tukui/Modules/DataTexts/Core.lua
@@ -210,7 +210,7 @@ function TukuiDT:AddDefaults()
 	TukuiData[GetRealmName()][UnitName("player")].Texts["Durability"] = {true, 2}
 	TukuiData[GetRealmName()][UnitName("player")].Texts["Friends"] = {true, 3}
 	TukuiData[GetRealmName()][UnitName("player")].Texts["System"] = {true, 4}
-	TukuiData[GetRealmName()][UnitName("player")].Texts["Bags Slots"] = {true, 5}
+	TukuiData[GetRealmName()][UnitName("player")].Texts["Micro Menu"] = {true, 5}
 	TukuiData[GetRealmName()][UnitName("player")].Texts["Gold"] = {true, 6}
 
 	if UnitLevel("player") == MAX_PLAYER_LEVEL then
